d6b0a4a92b418e631c409fb795e44c86704d1cb8
butterbot <124148649+butterbot6283@users.noreply.github.com>
2025-09-16 23:00:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,18 @@
 // Leave everything empty '' for default settings
 module.exports = {
-    useCustomId: false, 
-    customId: '', // your Discord app ID from developer portal
-    imdb_id: '', // IMDB ID fallback for poster & show title
-    mal_id: '', // MAL ID fallback for poster & show title
-    autoPoster: false, // fetch poster by filename (not always accurate)
-    customText: '', // replace title with custom text
-    customBigText: '', // replace large image text
-    customType: '', // 0 = name, 1 = state, 2 = details
-    customImage: [ // replace MPC logo with your custom images
+    customId: '', // Your Discord application ID from the developer portal
+    imdb_id: '', // IMDb ID fallback for fetching poster and show title
+    mal_id: '', // MyAnimeList ID fallback for fetching poster and show title
+    customText: '', // Replace video title with custom text in Discord presence
+    customBigText: '', // Replace large image text in Discord presence
+    useCustomId: false, // Use custom Discord app ID if true, otherwise use default
+    autoPoster: false, // Fetch poster by filename if true (may be inaccurate)
+    cleanFilename: true, // Clean video filename using regex patterns if true
+    customImage: [ // Replace MPC logo with custom image URLs
         ''
     ],
+    cleanRegex: [ // Custom regex patterns to clean video filename
+        '\\b(720p|1080p|480p|BluRay|BRRip|Webrip|WEB-DL|WEBDL|HDRip|x264|x265|HEVC|HDTV|DVDRip|AAC)\\b\\s*'
+    ],
+    customType: '', // Discord presence type (0 = name, 1 = state, 2 = details, leave empty for auto)
 };
